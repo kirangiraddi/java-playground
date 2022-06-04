3cb7a7753ff7949d6758771eb750aec8f4cc6157
kirangiraddi <kiranmgiraddi@gmail.com>
2022-06-04 17:02:14 +0530

    Added HelloWorld.java

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    //printing a string
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
