4d156d3eea331621b41150699ec1bf4807d06011
kirangiraddi <kiranmgiraddi@gmail.com>
2022-09-06 13:02:06 +0530

    Cleanup

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,8 +1,6 @@
 package com.example.helloworld;
 
 public class HelloWorld {
-    //printing a string
     public static void main(String[] args) {
-        System.out.println("Hello World");
     }
 }
